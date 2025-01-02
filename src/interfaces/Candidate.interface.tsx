a4335c3d25fee1151ac8a251d32427b9af6b9f42
ShirishaKb <shirishakb@gmail.com>
2025-01-02 00:09:32 -0500

    modified few files

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,11 +1,12 @@
 export interface Candidate {
     id: number | null;
     login: string | null;
-    email: string | null;
-    html_url: string | null;
-    name: string | null;
-    avatar_url: string | null;
-    bio: string | null;
-    company: string | null;
-    location: string | null;
-    }
+    email?: string | null;
+    html_url?: string | null;
+    name?: string | null;
+    bio?: string | null;
+    company?: string | null;
+    location?: string | null;
+    avatar_url?: string | null;
+  }
+  
